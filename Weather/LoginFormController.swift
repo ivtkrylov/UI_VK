d1baf622dcdc1f754290fac93198911f28e89eb0
ivtkrylov <ivtkrylov@gmail.com>
2021-11-01 12:37:53 +0300

    1) первичная настройка цветов 2) констрейнты по расположению элементов

---
+++ Weather/LoginFormController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginFormController.swift
+//  Weather
+//
+//  Created by Иван Крылов on 27.10.2021.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
